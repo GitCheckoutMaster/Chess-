49e4d0bc64036f6f14d7e7bdbad74db44cae67d2
Jay <133483880+GitCheckoutMaster@users.noreply.github.com>
2025-04-23 15:20:35 +0530

    updated the color of the board when piece moves

--- board.py
+++ board.py
@@ -5,6 +5,8 @@ class Board:
   def __init__(self):
     self.active_piece = None
     self.active_square = None
+    self.active_color = (238,238,210)
+    self.highlighted_square = []
     self.FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
     self.board = [""] * 64
     # self.board = [
@@ -25,7 +27,8 @@ class Board:
         color = (255, 255, 255)
         if (i + j) % 2 != 0:
           color = (118, 150, 86)
-        pygame.draw.rect(screen, color, (j * 60, i * 60, 60, 60))
+        idx = i * 8 + j
+        pygame.draw.rect(screen, self.active_color if idx in self.highlighted_square else color, (j * 60, i * 60, 60, 60))
 
         # Draw the pieces
         piece = self.board[i * 8 + j]
@@ -82,6 +85,7 @@ class Board:
 
     piece = self.board[index]
     if piece != "":
+      self.highlight_square(index, None, False)
       self.active_piece = piece
       self.active_square = index
       self.board[index] = ""  # Remove the piece from the board
@@ -92,34 +96,48 @@ class Board:
     mouse_x, mouse_y = pygame.mouse.get_pos()
     row = mouse_y // 60
     col = mouse_x // 60
-    index = row * 8 + col
-    piece = self.board[index]
+    index = row * 8 + col  
 
-    if self.active_piece is not None and ((self.active_piece.isupper() and self.FEN.split(" ")[1] == "b") or (self.active_piece.islower() and self.FEN.split(" ")[1] == "w")):
+    if self.active_piece is not None and ((self.active_piece.isupper() and self.FEN.split(" ")[1] == "b") or (self.active_piece.islower() and self.FEN.split(" ")[1] == "w") or (index < 0 or index >= 64)):
       self.board[self.active_square] = self.active_piece
       self.active_piece = None
       self.active_square = None
       self.update_fen()
+      self.highlight_square(self.highlighted_square[0], self.highlighted_square[1] if len(self.highlighted_square) > 1 else None)
       return
+
+    piece = self.board[index]
 
     if self.active_piece is not None:
       if piece == "":
         self.board[index] = self.active_piece
+        self.highlight_square(self.active_square, index)
         self.change_turn()
       else:
         active_piece_color = 8 if self.active_piece.isupper() else 16
         target_piece_color = 8 if piece.isupper() else 16
         if active_piece_color != target_piece_color:
           self.board[index] = self.active_piece
+          self.highlight_square(self.active_square, index)
           self.change_turn()
         else:
           self.board[self.active_square] = self.active_piece
+          self.highlight_square(self.highlighted_square[0], self.highlighted_square[1] if len(self.highlighted_square) > 1 else None)
       
       self.active_piece = None
       self.active_square = None
     self.update_fen()
 
+  def highlight_square(self, active_square=None, target_square=None, clear=True):
+    if clear:
+      self.highlighted_square = []
+    if active_square is not None:
+      self.highlighted_square.append(active_square)
+    if target_square is not None:
+      self.highlighted_square.append(target_square)
+
   def change_turn(self):
+    print("Changing turn")
     if self.FEN.split(" ")[1] == "w":
       self.FEN = self.FEN.replace("w", "b")
     else:
---
+++ moves.py
@@ -0,0 +1,6 @@
+class Moves:
+  def __init__(self):
+    self.legal_moves = []
+
+  def generate_legal_moves_for_sliding_pieces(self):
+    pass
